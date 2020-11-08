c5741860aee8f292bba037f0c18e455578d9c315
Warppig <45257998+Warppig@users.noreply.github.com>
2020-11-08 19:00:43 +1000

    Fart
    braaaap

--- js/used.js
+++ js/used.js
@@ -15,3 +15,53 @@ function faviFlip() {
 }
 
 setInterval(faviFlip, 1000);
+//
+
+//Get it
+function GetElmntInCon(containerID, childID) {
+    var elm = document.getElementById(childID);
+    var parent = elm ? elm.parentNode : {};
+    return (parent.id && parent.id === containerID) ? elm : {};
+}
+//
+
+//Illegal Iframage
+function rsif(obj) {
+
+}
+//
+
+//Every day I'm toggling
+function C_Tog(obj, str) {
+	var cls = obj.parentElement.querySelector('div');
+	
+	var boo = (cls.style.display === "none");
+	
+	if (boo) {
+		cls.style = 'display: block;';
+		obj.n ='a'
+		obj.innerHTML = 'Close '+str;
+	} else {
+		cls.style.display = 'none';
+		obj.innerHTML = 'Open '+str;
+	}
+	
+	return boo;
+}
+//Frame toggle
+function C_TogFrm(obj, str) {
+	var frm = obj.parentElement.querySelector('div').querySelector('iframe');
+	var set = obj.link
+	console.log(obj.link);
+	var b = C_Tog(obj, str);
+	
+	if (b) {
+		frm.src = set;
+	} else {
+		frm.src = '';
+	}
+}
+
+
+
+
